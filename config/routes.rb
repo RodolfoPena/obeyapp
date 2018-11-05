9c4dba011e2e506014956c4a2245cbfe428cfacd
Rodolfo <rodolfo@MacBook-Pro-de-Rodolfo.local>
2018-11-05 20:04:50 -0300

    Stylesheet improvement for: Index Why Obeya Prices Obeya Global

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'pages/index'
+  get 'pages/prices'
+  get 'pages/evolution'
+  get 'pages/certification'
+  get 'pages/obeya_global'
+  get 'pages/prototipe'
   root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
